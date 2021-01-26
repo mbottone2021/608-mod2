22dae8f1fe445bd789b561d64caa656e5f3ff6e2
mbottone2021 <77653069+mbottone2021@users.noreply.github.com>
2021-01-25 22:04:57 -0600

    Update central-with-statistics.py

--- central-with-statistics.py
+++ central-with-statistics.py
@@ -1,1 +1,9 @@
-
+import statistics
+values = [47, 95, 88, 73, 88, 84]
+a = len(values)
+b = sum(values)
+c = statistics.mean(values)
+d = statistics.median(values)
+e = statistics.mode(values)
+print(a, b, c, d, e)
+6 475 79.16666666666667 86.0 88
